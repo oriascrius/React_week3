1f776e56d1b746e8d85dcc438001164b200974b0
oriascrius <qoq77416416@gmail.com>
2025-01-25 20:48:57 +0800

    更改vite設定，base改檔名

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// 設定 base 路徑
 export default defineConfig({
   base: '/React_week3/',
   plugins: [react()]
